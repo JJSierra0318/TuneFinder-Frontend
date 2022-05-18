765c57de0be056430c0d6353e4edb74d4eef21d9
JJSierra0318 <juanjosesierram@gmail.com>
2022-05-17 21:24:09 -0500

    feat: Get user's credentials when they log in

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
-import Home from "./components/Home";
 import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
 import { useDispatch, useSelector } from "react-redux";
 import { userLogout } from "./reducers/tokenReducer";
+import axios from 'axios';
 
 import LogIn from "./components/LogIn";
+import Home from "./components/Home";
+
+const getUser = async (token) => {
+  const {data} = await axios({
+    method: 'get',
+    url: 'https://api.spotify.com/v1/me',
+    withCredentials: false,
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  })
+
+  return data
+}
 
 const App = () => {
 
   const dispatch = useDispatch()
   const token = useSelector(state => state.token)
+  console.log(token)
+  let user = ''
+
+    if (token) {
+      user = getUser(token)
+    }
+    
+
 
   const padding = {
     padding: 5
@@ -20,6 +42,7 @@ const App = () => {
         {token
         ? <div className="menu">
             <button><Link style={padding} to="/home">home</Link></button>
+            <></>
             <button id='right' onClick={() => {
               dispatch(userLogout())
               window.localStorage.setItem('token', '')}}>
