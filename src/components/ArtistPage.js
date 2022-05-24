f38a4295da59082365945658db11c891d1ffbce2
JJSierra0318 <juanjosesierram@gmail.com>
2022-05-23 20:35:14 -0500

    feat: Added general artist's name, genres and followers to infoPage

--- src/components/ArtistPage.js
+++ src/components/ArtistPage.js
@@ -1,9 +1,53 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+import { useSelector } from "react-redux"
+import { useParams } from "react-router-dom"
+import User from '../images/User.png'
+
 const ArtistPage = () => {
+
+  const [artist, setArtist] = useState('')
+  const token = useSelector(state => state.token)
+
+  const fetchArtist = async () => {
+    const {data} = await axios({
+      method: 'get',
+      url: `https://api.spotify.com/v1/artists/${id}`,
+      withCredentials: false,
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    })
+
+    setArtist(data)
+  }
+
+  const id = useParams().id
+
+  useEffect(() => {
+    if (!artist) fetchArtist()
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+    
+  console.log(artist)
+
+
+  if (!token || !artist) return null
+
   return (
     <div className="searchPage">
       <div className="search">
-        <h2>Artist Name</h2>
-        <p>artist info</p>
+        <div className="generalInfo">
+        {artist.images.length > 0
+          ? <img src={artist.images[0].url} alt='Artist Logo'/>
+          : <img src={User} alt='Artist Logo'/>}
+          <h2>{artist.name}</h2>
+          <hr />
+          {artist.genres.length > 0
+          ? <p><strong>Genres: </strong>{artist.genres.map(genre => <em key={genre}>{genre} / </em>)}</p>
+          : null}
+          <p><strong>Followers:  </strong><em>{artist.followers.total}</em></p>
+        </div>
       </div>
     </div>
   )
