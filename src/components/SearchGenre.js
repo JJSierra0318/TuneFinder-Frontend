68a3c364fab17b249cec01b3acb3bbee4dbb900e
JJSierra0318 <juanjosesierram@gmail.com>
2022-05-27 20:29:22 -0500

    style: Designed Search by Genre page

--- src/components/SearchGenre.js
+++ src/components/SearchGenre.js
@@ -1,7 +1,29 @@
+import { useState } from "react"
+
 const SearchGenre = () => {
+
+  const [search, setSearch] = useState('')
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+  }
+
   return(
-    <div>
-      TEST
+    <div className="searchPage">
+      <div className="search">
+        <center>
+          <h1>Find By Genre</h1>
+          <form onSubmit={onSubmit}>
+            <div className="input">
+            <input
+              placeholder="Genre name"
+              onChange={(e) => setSearch(e)}
+            />
+            <button type="submit">Search</button>
+            </div>
+          </form>
+        </center>
+      </div>
     </div>
   )
 }
