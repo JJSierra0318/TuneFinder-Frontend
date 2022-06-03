9e6c3add46aa934b80a8a34083c30d0326f63e69
JJSierra0318 <juanjosesierram@gmail.com>
2022-06-03 17:16:29 -0500

    feat: Replicated tracks search on SearchArtist.js and updated title

--- src/components/SearchArtist.js
+++ src/components/SearchArtist.js
@@ -2,12 +2,16 @@ import axios from "axios"
 import { useState } from "react"
 import { useSelector } from "react-redux"
 import ArtistList from "./SearchResult/ArtistList"
+import TrackList from "./SearchResult/TrackList"
 
 const SearchArtist = () => {
 
   const [search, setSearch] = useState('')
-  const [artists, setArtists] = useState('')
+  const [filter, setFilter] = useState('')
 
+  const [artists, setArtists] = useState('')
+  const [tracks, setTracks] = useState('')
+  const [playlists, setPlaylists] = useState('')
   const loggedIn = useSelector(state => state.token)
   if (!loggedIn) return null
 
@@ -22,18 +26,39 @@ const SearchArtist = () => {
       },
       params: {
         q: search,
-        type: 'artist'
+        type: filter
       }
     })
-    setArtists(data.artists.items)
+
+    switch (filter) {
+      case 'artist':
+        setArtists(data.artists.items)
+        break
+      case 'track':
+        console.log(data)
+        setTracks(data.tracks.items)
+        break
+      case 'playlist':
+        console.log('playlist')
+        break
+      default:
+        setArtists(data.artists.items)
+    }
   }
 
   return (
     <div className='searchPage'>
       <div className='search'>
         <center>
-          <h1>Find by Artist</h1>
+          <h1>Find by Name</h1>
           <form onSubmit={onSubmit}>
+            <div className="filter">
+              <center>
+                <button onClick={() => setFilter('artist')}>Artists</button>
+                <button onClick={() => setFilter('track')}>Tracks</button>
+                <button onClick={() => setFilter('playlist')}>Playlists</button>
+              </center>
+            </div>
             <div className="input">
               <input
                 placeholder="Artist name"
@@ -43,8 +68,11 @@ const SearchArtist = () => {
             </div>
           </form>
         </center>
-        {artists
+        {filter === 'artist' && artists
           ? <ArtistList artists={artists} />
+          : null}
+        {filter === 'track' && tracks
+          ? <TrackList tracks={tracks}/>
           : null}
       </div>
     </div>
