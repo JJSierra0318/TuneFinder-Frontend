d628de132ff9b86e2a30749cae27063bab00e9e0
JJSierra0318 <juanjosesierram@gmail.com>
2022-06-03 17:09:51 -0500

    feat: Show tracks on screen according to the given genre

--- src/components/SearchResult/TrackList.js
+++ src/components/SearchResult/TrackList.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom"
 import Note from '../../images/Note.png'
 
 const TrackList = ({tracks}) => {
@@ -7,6 +8,11 @@ const TrackList = ({tracks}) => {
         {track.album.images.length > 0
           ? <img src={track.album.images[0].url} alt='album logo'/>
           : <img src={Note} alt='album logo'/>}
+          <h2>{track.name}</h2>
+          <hr></hr>
+          <p><strong>Artists: </strong>{track.artists.map(artist => <Link key={artist.id} to={`/artist/${artist.id}`}>{artist.name} / </Link>)}</p>
+          <p><strong>Album: </strong>{track.album.name}</p>
+          <p><strong>Duration: </strong>{Math.floor((track.duration_ms/1000)/60)}:{Math.floor((track.duration_ms/1000)%60)} min</p>
       </div>)}
     </div>
   )
