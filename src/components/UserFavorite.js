54080f09e65c5b9658acb24a360ae330899c3f2d
JJSierra0318 <juanjosesierram@gmail.com>
2022-06-06 22:25:34 -0500

    style: Initial style for User's favorite artists page

--- src/components/UserFavorite.js
+++ src/components/UserFavorite.js
@@ -1,4 +1,18 @@
+import { useSelector } from "react-redux"
+
 const UserFavorite = () => {
+
+  const token = useSelector(state => state.token)
+
+  if (!token) return null
+
+  return(
+    <div className="searchPage">
+      <div className="search">
+        <h2>Favorite Artists</h2>
+      </div>
+    </div>
+  )
 
 }
 
